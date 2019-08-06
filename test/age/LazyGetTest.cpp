d3eb47ca3168f32c386f7ef38421c950f7536ac0
Sarah Sophie Stieß <st115875@stud.uni-stuttgart.de>
2019-08-06 13:56:19 +0200

    added test wrt to get on lazyfield

---
+++ test/age/LazyGetTest.cpp
@@ -0,0 +1,32 @@
+#include <gtest/gtest.h>
+#include "../common/utils.h"
+#include "../../src/age/File.h"
+#include "../../src/empty/File.h"
+
+#include "../../lib/ogss.common.cpp/ogss/iterators/StaticFieldIterator.h"
+
+
+using namespace common;
+
+/*
+ *
+ */
+TEST(LazyGetTest, test0) {
+    try {
+        auto sf = common::tempFile<age::api::File>();
+        auto a = sf->Age->make();
+        sf->close();
+
+        std::unique_ptr<empty::api::File> sf2(empty::api::File::open(sf->currentPath()));
+        auto pool = sf2->pool(sf2->strings->add(u8"Age"));
+        ASSERT_EQ(1, pool->size());
+        auto obj = pool->getAsAnnotation(a->ID());
+        auto f = pool->fields().next();
+
+        f->setR(obj, ::ogss::box(42));
+
+    } catch (ogss::Exception &e) {
+        GTEST_FAIL() << "an exception was thrown:" << std::endl << e.what();
+    }
+    GTEST_SUCCEED();
+}
